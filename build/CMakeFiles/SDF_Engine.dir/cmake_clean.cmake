90aee5c25659b2745a05d4c1decc371c783df22a
Hunter Liles <hunterliles06@gmail.com>
2026-04-08 17:15:51 -0500

    Ported over a lot of Danes original stuff. Replaced the lighting, raymarcher, and added sdf 1, 2, 3. Also, NoiseSDF now works.

---
+++ build/CMakeFiles/SDF_Engine.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDF_Engine.dir/link.d"
+  "CMakeFiles/SDF_Engine.dir/_deps/imgui-src/imgui.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/_deps/imgui-src/imgui.cpp.o.d"
+  "CMakeFiles/SDF_Engine.dir/_deps/imgui-src/imgui_draw.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/_deps/imgui-src/imgui_draw.cpp.o.d"
+  "CMakeFiles/SDF_Engine.dir/_deps/imgui-src/imgui_tables.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/_deps/imgui-src/imgui_tables.cpp.o.d"
+  "CMakeFiles/SDF_Engine.dir/_deps/imgui-src/imgui_widgets.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/_deps/imgui-src/imgui_widgets.cpp.o.d"
+  "CMakeFiles/SDF_Engine.dir/_deps/rlimgui-src/rlImGui.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/_deps/rlimgui-src/rlImGui.cpp.o.d"
+  "CMakeFiles/SDF_Engine.dir/src/gui.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/src/gui.cpp.o.d"
+  "CMakeFiles/SDF_Engine.dir/src/main.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/src/main.cpp.o.d"
+  "CMakeFiles/SDF_Engine.dir/src/movement.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/src/movement.cpp.o.d"
+  "CMakeFiles/SDF_Engine.dir/src/shaderLoad.cpp.o"
+  "CMakeFiles/SDF_Engine.dir/src/shaderLoad.cpp.o.d"
+  "SDF_Engine"
+  "SDF_Engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDF_Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
